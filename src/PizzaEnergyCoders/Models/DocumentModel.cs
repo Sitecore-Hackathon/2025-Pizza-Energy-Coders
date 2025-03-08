9dc87c16a50ca8f8fe151b8347a2b1c89b84205b
Jonathan Lemus <jlemus@oshyn.com>
2025-03-07 23:19:49 -0500

    Added new Models/DocumentModel.cs

---
+++ src/PizzaEnergyCoders/Models/DocumentModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaEnergyCoders.Models
+{
+    public class DocumentModel
+    {
+        public string TemplateId { get; set; }
+        public string Title { get; set; }
+        public Dictionary<string, string> KeyValues { set; get; }
+    }
+}
